7fd50650d1039e96fcdc59c0a4494868c2c7b949
Kyrylo Lapchenko <kyrylo.lap@gmail.com>
2024-12-07 14:34:23 +0100

    Add helath and metric endpoint

--- handlers.go
+++ handlers.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+    "fmt"
+    "io"
 	"net/http"
 )
 
-func handleHeatlthz(w http.ResponseWriter, req *http.Request) {
+func handlerHeatlthz(w http.ResponseWriter, req *http.Request) {
     w.Header().Set("Content-Type", "text/plain; charset=utf-8")
     w.Write([]byte("OK"))
     w.WriteHeader(http.StatusOK)
-} 
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    hits := cfg.reqCount.Load()
+    io.WriteString(w, fmt.Sprintf(
+    `<html>
+      <body>
+        <h1>Welcome, Chirpy Admin</h1>
+        <p>Chirpy has been visited %d times!</p>
+      </body>
+    </html>`,
+    hits))
+    w.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+    cfg.reqCount.Store(0)
+    w.WriteHeader(http.StatusOK)
+}
--- main.go
+++ main.go
@@ -3,12 +3,23 @@ package main
 import (
     "fmt"
     "net/http"
+    "sync/atomic"
 )
+
+type apiConfig struct {
+    reqCount atomic.Int32 
+}
 
 func main() {
     serveMux := http.NewServeMux()
-    serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-    serveMux.Handle("/healthz", http.HandlerFunc(handleHeatlthz))
+    cfg := apiConfig{ reqCount: atomic.Int32{} }
+
+    appHandler :=  http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+    serveMux.Handle("/app/", cfg.middlewareMetricsInc(appHandler))
+    serveMux.Handle("GET /api/healthz", http.HandlerFunc(handlerHeatlthz))
+    serveMux.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
+    serveMux.HandleFunc("POST /admin/reset", cfg.handlerReset)
+
     server := http.Server{ Handler: serveMux, Addr: ":8080" }
     err := server.ListenAndServe()
     if err != nil {
