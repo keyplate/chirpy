9296751a7c139e5d8ce974817a0f9500a35327a1
Kyrylo Lapchenko <kyrylo.lap@gmail.com>
2024-12-07 18:46:28 +0100

    Clean up move admin handlers to separate file

---
+++ handlersAdmin.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "io"
+)
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    hits := cfg.reqCount.Load()
+    io.WriteString(w, fmt.Sprintf(
+    `<html>
+      <body>
+        <h1>Welcome, Chirpy Admin</h1>
+        <p>Chirpy has been visited %d times!</p>
+      </body>
+    </html>`,
+    hits))
+    w.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+    cfg.reqCount.Store(0)
+    w.WriteHeader(http.StatusOK)
+}
