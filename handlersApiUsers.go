2a6089969493d092c4f73cc0b4a4838f8271f9fd
Kyrylo Lapchenko <kyrylo.lap@gmail.com>
2024-12-12 13:29:52 +0100

    Add password to users for creation and login

--- handlersApiUsers.go
+++ handlersApiUsers.go
@@ -6,12 +6,19 @@ import (
     "net/http"
     "time"
 
+    "github.com/keyplate/chirpy/internal/auth"
     "github.com/keyplate/chirpy/internal/database"
     "github.com/google/uuid"
 )
 
-type createUserBody struct {
+type createUserRequest struct {
     Email string `json:"email"`
+    Password string `json:"password"` 
+}
+
+type loginUserRequest struct {
+    Email string `json:"email"`
+    Password string `json:"password"`
 }
 
 type usrResponse struct {
@@ -24,21 +31,30 @@ type usrResponse struct {
 func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, req *http.Request) {
     defer req.Body.Close()
 
+    var usrReq createUserRequest
     dat, err := io.ReadAll(req.Body)
-    var usrEmail createUserBody
-
     if err != nil {
         respondWithError(w, 400, "Can not read body")
         return
     }
 
-    err = json.Unmarshal(dat, &usrEmail)
+    err = json.Unmarshal(dat, &usrReq)
     if err != nil {    
         respondWithError(w, 400, "Can not read body")
         return
     }
     
-    usrParams := database.CreateUserParams{ ID: uuid.New(), CreatedAt: time.Now(), UpdatedAt: time.Now(), Email: usrEmail.Email }
+    hashedPass, err := auth.HashPassword(usrReq.Password)
+    if err != nil {
+        respondWithError(w, 400, "Something went wrong")
+    }
+    usrParams := database.CreateUserParams{ 
+        ID: uuid.New(), 
+        CreatedAt: time.Now(),
+        UpdatedAt: time.Now(),
+        Email: usrReq.Email,
+        HashedPassword: hashedPass,
+    }
     usr, err := cfg.db.CreateUser(req.Context(), usrParams)
     if err != nil {
         respondWithError(w, 500, err.Error())
@@ -47,4 +63,41 @@ func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, req *http.Request
 
     usrResp := usrResponse{ ID: usr.ID, CreatedAt: usr.CreatedAt, UpdatedAt: usr.UpdatedAt, Email: usr.Email }
     respondWithJSON(w, 201, usrResp)
+}
+
+func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, req *http.Request) {
+    defer req.Body.Close()
+
+
+    var loginReq loginUserRequest
+    dat, err := io.ReadAll(req.Body)
+    if err != nil {
+        respondWithError(w, 400, "Can not read body")
+        return
+    }
+
+    err = json.Unmarshal(dat, &loginReq)
+    if err != nil {
+        respondWithError(w, 400, "Can not read body")
+        return
+    }
+
+    usr, err := cfg.db.GetUserByEmail(req.Context(), loginReq.Email)
+    if err != nil {
+        respondWithError(w, 401, "Unauthorized")
+        return
+    }
+
+    err = auth.CheckPasswordHash(loginReq.Password, usr.HashedPassword)
+    if err != nil {
+        respondWithError(w, 401, "Unauthorized")
+        return
+    }
+
+    respondWithJSON(w, 200, usrResponse{
+        ID: usr.ID,
+        CreatedAt: usr.CreatedAt,
+        UpdatedAt: usr.UpdatedAt,
+        Email: usr.Email,
+    })
 }
--- main.go
+++ main.go
@@ -42,7 +42,9 @@ func main() {
     serveMux.HandleFunc("GET /api/chirps", cfg.handlerGetChirps)
     serveMux.HandleFunc("GET /api/chirps/{chirpID}", cfg.handlerGetChirpByID)
     serveMux.HandleFunc("POST /api/chirps", cfg.handlerCreateChirp)
+
     serveMux.HandleFunc("POST /api/users", cfg.handlerCreateUser)
+    serveMux.HandleFunc("POST /api/login", cfg.handlerLogin)
 
     server := http.Server{ Handler: serveMux, Addr: ":8080" }
     err = server.ListenAndServe()
